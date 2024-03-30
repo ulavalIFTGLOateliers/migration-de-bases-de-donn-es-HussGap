1828c7c980b5238d13d6ca0de163ba73dcd8b1e3
HussGap <HUZAR1@ulaval.ca>
2024-03-30 18:02:57 -0400

    Message de commit

--- database.py
+++ database.py
@@ -11,12 +11,17 @@ class Database:
         """
             Chargez les variables d'environnement de votre fichier .env, puis complétez les lignes 15 à 19 afin de récupérer les valeurs de ces variables
         """
+        load_dotenv()
+        self.host =os.environ.get("HOST")
 
-        self.host =
-        self.port =
-        self.database =
-        self.user =
-        self.password =
+        self.port =os.environ.get("PORT")
+
+        self.database =os.environ.get("DATABASE")
+
+        self.user =os.environ.get("USER")
+
+        self.password =os.environ.get("PASSWORD")
+
 
         self._open_sql_connection()
 
@@ -25,7 +30,7 @@ class Database:
     def _open_sql_connection(self):
         self.connection = pymysql.connect(
             host=self.host,
-            port=self.port,
+            port=int(self.port),
             user=self.user,
             password=self.password,
             db=self.database,
